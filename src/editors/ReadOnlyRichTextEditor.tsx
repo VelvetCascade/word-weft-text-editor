5cfe4fda7fe3b2a45111995c802a2fe16e42b6f0
Srijib <srijibbose@gmail.com>
2025-04-06 11:10:23 +0530

    reader page

--- src/editors/ReadOnlyRichTextEditor.tsx
+++ src/editors/ReadOnlyRichTextEditor.tsx
@@ -5,6 +5,9 @@ import {Editor} from "@tiptap/react";
 export const ReaderRichTextEditor: React.FC = () => {
 
     const editorRef = useRef<Editor | null>(null)
+
+    const messageQueue = useRef<any[]>([]);
+
     // State to store the content received from Wix
     const [content] = useState({
         "type": "doc",
@@ -23,8 +26,16 @@ export const ReaderRichTextEditor: React.FC = () => {
 
     const handleCreate = useCallback(
         ({editor}: { editor: Editor }) => {
-            if (editor.isEmpty) {
+            if (editor.isEmpty && messageQueue.current.length == 0) {
+                console.log("Editor Is empty and no queue data present");
                 editor.commands.setContent(content)
+                console.log("Default Content Successfully set");
+            }
+            else if(editor.isEmpty){
+                const queuedData = messageQueue.current.shift();
+                console.log("Editor Is empty but queue data present", queuedData);
+                editor.commands.setContent(queuedData);
+                console.log("Content Successfully set");
             }
             editorRef.current = editor
         },
@@ -43,11 +54,19 @@ export const ReaderRichTextEditor: React.FC = () => {
                 return;
             }
 
+            console.log("Editor is:", editorRef.current);
+            console.log("Received content from Wix:", event.data);
+
             if (event.data) {
                 // Update content state with data received from Wix
-                editorRef.current?.commands.setContent(event.data);
-                console.log(editorRef.current)
-                console.log("Received content from Wix:", event.data);
+                if(editorRef.current){
+                    console.log("Updating editor with data: ", event.data);
+                    editorRef.current.commands.setContent(event.data);
+                }
+                else{
+                    console.log("Pushing Data to Queue", event.data);
+                    messageQueue.current.push(event.data);
+                }
             }
         };
 
@@ -65,8 +84,7 @@ export const ReaderRichTextEditor: React.FC = () => {
             {...{description: ""}}
             throttleDelay={0}
             editorContentClassName="flex-1"
-            content={content}
-            output="html"
+            output="json"
             placeholder="Type your description here..."
             onCreate={handleCreate}
             autofocus={true}
