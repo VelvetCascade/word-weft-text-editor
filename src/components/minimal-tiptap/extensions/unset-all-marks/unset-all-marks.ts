5059f66920b50d3aa96089aa439c032ea259f404
di2001mi <dikshamishra500dm@gmail.com>
2025-04-19 20:29:54 +0530

    Toolbar Ui changes

--- src/components/minimal-tiptap/extensions/unset-all-marks/unset-all-marks.ts
+++ src/components/minimal-tiptap/extensions/unset-all-marks/unset-all-marks.ts
@@ -1,4 +1,6 @@
 import { Extension } from '@tiptap/react'
+import { FontFamily } from '@tiptap/extension-font-family'
+import Details from "@tiptap-pro/extension-details";
 
 export const UnsetAllMarks = Extension.create({
   addKeyboardShortcuts() {
@@ -6,4 +8,26 @@ export const UnsetAllMarks = Extension.create({
       'Mod-\\': () => this.editor.commands.unsetAllMarks()
     }
   }
+})
+
+export const CustomFontFamily = FontFamily.extend({
+  addKeyboardShortcuts() {
+    return {
+      // ↓ your new keyboard shortcut
+      'Mod-shift-1': () => this.editor.chain().focus().setFontFamily("sans-serif").run(),
+      'Mod-shift-2': () => this.editor.chain().focus().setFontFamily("serif").run(),
+      'Mod-shift-3': () => this.editor.chain().focus().setFontFamily("monospace").run(),
+      'Mod-shift-4': () => this.editor.chain().focus().setFontFamily("cursive").run(),
+      'Mod-shift-5': () => this.editor.chain().focus().setFontFamily("fantasy").run()
+    }
+  },
+})
+
+export const CustomDetails = Details.extend({
+  addKeyboardShortcuts() {
+    return {
+      // ↓ your new keyboard shortcut
+      'Mod-alt-D': () => this.editor.chain().focus().setDetails().run()
+    }
+  },
 })
